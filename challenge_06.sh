4735afdb7e310f0a36b8c2ca17b9f23caf4dedd7
Jenae Bierlink <116379162+j-bierlink@users.noreply.github.com>
2022-11-15 09:35:41 -0800

    Update challenge_06.sh

--- challenge_06.sh
+++ challenge_06.sh
@@ -5,7 +5,7 @@
 # Date of latest revision: 11/14/22     
 # Purpose: Conditionals  
 
-file=newtestfile.sh
+file='newtestfile.sh newtestfile2.sh'
 
 # Create a script that detects if a file or directory exists, then creates it if it does not exist.
 
@@ -18,3 +18,8 @@ else
     touch $file
 fi
 # Your script must use at least one array, one loop, and one conditional.
+
+for file in $file
+do
+        echo $file
+done
