648b73189883da67a0aac5521f4debd4e33e954e
Jenae Bierlink <116379162+j-bierlink@users.noreply.github.com>
2022-11-14 17:24:13 -0800

    Create challenge_06.sh

---
+++ challenge_06.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script: challenge 06                       
+# Author: Jenae Bierink                      
+# Date of latest revision: 11/14/22     
+# Purpose: Conditionals  
+
+file=newtestfile.sh
+
+# Create a script that detects if a file or directory exists, then creates it if it does not exist.
+
+if
+    test -f "$file"
+then
+    echo "$file exists"
+else
+
+    touch $file
+fi
+# Your script must use at least one array, one loop, and one conditional.
