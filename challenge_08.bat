cc05e1c77e8a5142790e06e25c59dde24e992f1e
Jenae Bierlink <116379162+j-bierlink@users.noreply.github.com>
2022-11-17 10:33:03 -0800

    Update and rename challenge_08 to challenge_08.bat

---
+++ challenge_08.bat
@@ -0,0 +1,14 @@
+:: Windows .bat file
+
+:: Script:  challenge_08                    
+:: Author:  Jenae Bielrink                     
+:: Date of latest revision: 11/17/22     
+:: Purpose: Batch Files                     
+
+
+ROBOCOPY "C:\Users\j.thomson\Desktop\My Project File" "E:\Copied project files" /e
+
+:: pause
+
+:: I saved the batch file to my desktop and scheduled the batch file to run the script 2 minutes after I clicked ok in the Task Scheduler. And it worked! 
+:: It copied the files from the desktop into the E: folder. 
