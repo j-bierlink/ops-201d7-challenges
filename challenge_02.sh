1958662ac0c508f74219d5a1ae171b13f70f7b4e
j-bierlink <jenaebierlink@gmail.com>
2022-11-21 14:49:49 +0000

    rename

---
+++ challenge_02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script Name:                  Class 02 Ops Challenge Solution
+# Class Name:                   Ops 201
+# Author Name:                  Jenae Bierlink
+# Date of latest revision:      11/9/22
+# Purpose:                      assigning variables
+
+var="I am working perfectly"
+
+echo $var
+
+# How would you "echo" out the contents of a file?
