eb14b202c8a239e2211b80eed032d226b0dbeba4
j-bierlink <jenaebierlink@gmail.com>
2022-11-21 14:52:42 +0000

    basic function - rename

---
+++ challenge_03.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script:                       
+# Author: jenae bierlink                      
+# Date of latest revision: 11/9/22     
+# Purpose:      
+
+# Basic function
+user="last"
+
+login_history () {
+  $user
+}
+
+login_history
