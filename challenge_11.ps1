34f6b90898984d7980ebfacbac668bf9098478b9
j-bierlink <jenaebierlink@gmail.com>
2022-11-22 18:15:40 +0000

    Automated End Point name update

---
+++ challenge_11.ps1
@@ -0,0 +1,27 @@
+# Script:  challenge_11                    
+# Author:  Jenae Bielrink                     
+# Date of latest revision: 11/21/22     
+# Purpose: Automated End Point Configuration  
+
+# https://github.com/superswan/Powershell-SysAdmin
+
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+# Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+
+# Enable Remote management
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+
+### End
